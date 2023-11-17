cca59cc2eea4fa8a30b001cec7de5fce6837ca88
yangzhou <yangzhou221@gmail.com>
2023-11-17 04:26:13 +0000

    add l1 genesis module

---
+++ x/l1/module_test.go
@@ -0,0 +1,31 @@
+package l1
+
+import (
+	"encoding/json"
+	"github.com/PlatONnetwork/AppChain-SDK/store/memorydb"
+	"github.com/PlatONnetwork/PlatON-Go/common"
+	"github.com/stretchr/testify/require"
+	"math/big"
+	"testing"
+)
+
+func TestModule(t *testing.T) {
+	store := memorydb.New()
+	l1 := NewL1(store)
+	g := Genesis{
+		ChainID:    big.NewInt(11),
+		State:      common.BigToAddress(big.NewInt(1)),
+		CheckPoint: common.BigToAddress(big.NewInt(2)),
+	}
+	raw, _ := json.Marshal(g)
+	l1.InitGenesis(nil, nil, nil, raw)
+	chainId, err := l1.db.GetChainID()
+	require.Nil(t, err)
+	require.Equal(t, g.ChainID, chainId)
+	addr, err := l1.db.GetStateAddress()
+	require.Nil(t, err)
+	require.Equal(t, g.State, addr)
+	addr, err = l1.db.GetCheckpointAddress()
+	require.Nil(t, err)
+	require.Equal(t, g.CheckPoint, addr)
+}
