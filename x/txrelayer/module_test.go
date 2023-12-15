85faa21c123eb1f3736758070c14c9b64a3f7b5d
mowind <wjinwen.1988@gmail.com>
2023-12-15 10:27:08 +0800

    Fixed test

--- x/txrelayer/module_test.go
+++ x/txrelayer/module_test.go
@@ -1,59 +1,33 @@
 package txrelayer
 
 import (
-	"context"
-	"crypto/ecdsa"
+	"fmt"
 	"math/big"
 	"testing"
 
+	"github.com/PlatONnetwork/PlatON-Go/accounts/keystore"
 	"github.com/PlatONnetwork/PlatON-Go/common"
 	"github.com/PlatONnetwork/PlatON-Go/core/types"
 	"github.com/PlatONnetwork/PlatON-Go/crypto"
+	"github.com/google/uuid"
 	"github.com/test-go/testify/assert"
+
+	"github.com/PlatONnetwork/AppChain-SDK/x/checkpoint/contractsapi"
 )
 
 var (
-	_ Signer = (*signerImpl)(nil)
-
-	privateKey  = "68e7f80b51de2c0761fcdccc43d4c07d94e19dff399feafc06c2db6af1610c2d"
-	CounterAddr = common.HexToAddress("0xdaCB886C825aF81d4361dF81Dc556ba017772ee7")
+	privateKeyHex = "68e7f80b51de2c0761fcdccc43d4c07d94e19dff399feafc06c2db6af1610c2d"
+	CounterAddr   = common.HexToAddress("0xdaCB886C825aF81d4361dF81Dc556ba017772ee7")
 )
 
-type signerImpl struct {
-	privateKey *ecdsa.PrivateKey
-	chainId    *big.Int
-	address    common.Address
-}
-
-func NewSigner(privateKeyHex string, chainId *big.Int) (Signer, error) {
-	pk, err := crypto.HexToECDSA(privateKeyHex)
-	if err != nil {
-		return nil, err
-	}
-	addr := crypto.PubkeyToAddress(pk.PublicKey)
-	return &signerImpl{
-		privateKey: pk,
-		chainId:    chainId,
-		address:    addr,
-	}, nil
-}
-
-func (s *signerImpl) SignTx(txn *types.Transaction) (*types.Transaction, error) {
-	signer := types.NewEIP155Signer(s.chainId)
-	return types.SignTx(txn, signer, s.privateKey)
-}
-
-func (s *signerImpl) Address() common.Address {
-	return s.address
-}
-
-func newTxRelayer(rpcAddr string) (*Module, error) {
-	return NewModule(rpcAddr, DefaultReceiptTimeout, DefaultNumRetries)
+func newTxRelayer(rpcAddr string) *Module {
+	txRelayer := NewModule(rpcAddr, DefaultReceiptTimeout, DefaultNumRetries)
+	txRelayer.Init()
+	return txRelayer
 }
 
 func TestCall(t *testing.T) {
-	m, err := newTxRelayer("https://devnet2openapi2.platon.network/rpc")
-	assert.Nil(t, err)
+	m := newTxRelayer("https://devnet2openapi2.platon.network/rpc")
 
 	data := common.Hex2Bytes("10c2044e")
 	wlat := common.HexToAddress("0xcc9fbab49c29b3ff536a3d94873e988cc4a572af")
@@ -65,17 +39,21 @@ func TestCall(t *testing.T) {
 }
 
 func TestSendTransaction(t *testing.T) {
-	m, err := newTxRelayer("https://devnet2openapi2.platon.network/rpc")
-	assert.Nil(t, err)
+	m := newTxRelayer("https://devnet2openapi2.platon.network/rpc")
+	err := m.Init()
+	assert.NoError(t, err)
 
-	chainId, err := m.client.ChainID(context.Background())
-	assert.Nil(t, err)
-
-	signer, err := NewSigner(privateKey, chainId)
-	assert.Nil(t, err)
+	privateKey := crypto.HexMustToECDSA(privateKeyHex)
+	id, err := uuid.NewRandom()
+	assert.NoError(t, err)
+	key := &keystore.Key{
+		Id:         id,
+		Address:    crypto.PubkeyToAddress(privateKey.PublicKey),
+		PrivateKey: privateKey,
+	}
 
 	numberData := common.Hex2Bytes("8381f58a")
-	result, err := m.Call(signer.Address(), CounterAddr, numberData)
+	result, err := m.Call(key.Address, CounterAddr, numberData)
 	assert.Nil(t, err)
 	number := big.NewInt(0).SetBytes([]byte(result))
 
@@ -87,7 +65,7 @@ func TestSendTransaction(t *testing.T) {
 		Data:     incrementData,
 		To:       &CounterAddr,
 		Value:    nil,
-	}), signer)
+	}), key)
 	assert.Nil(t, err)
 	assert.Equal(t, receipt.Status, types.ReceiptStatusSuccessful)
 
