324c779632f74c31815defa0d65e6df2e9860fa6
mowind <wjinwen.1988@gmail.com>
2023-12-26 11:08:01 +0800

    Remove log

--- x/staking/contracts/stake_handler_impl.go
+++ x/staking/contracts/stake_handler_impl.go
@@ -3,7 +3,6 @@ package contracts
 import (
 	"bytes"
 	"errors"
-	"fmt"
 
 	"github.com/PlatONnetwork/AppChain-SDK/core/contracts"
 	typesdk "github.com/PlatONnetwork/AppChain-SDK/types"
@@ -72,7 +71,6 @@ func (c *StakeHandler) GetDelegationsWithValidator(validators []common.Address, 
 	delegationQueue := make([]DelegationInfo, 0)
 	for _, validatorAddr := range validators {
 		epochs, _ := c.getValidatorDelegationRcPendingAndEpoch(validatorAddr, math.MaxUint64)
-		log.Info("Get delegations with validator", "epochs", fmt.Sprintf("%v", epochs))
 		for _, stakeEpoch := range epochs {
 			delegation := c.getDelegation(delegator, validatorAddr, stakeEpoch)
 			if delegation.IsEmpty() {
