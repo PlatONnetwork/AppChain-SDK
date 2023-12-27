e84131e9a9d80a19b8938a77e3f5dbb2f21e9ddb
GavinXu <meiyan532484710@gmail.com>
2023-12-27 16:32:35 +0800

    change sol for RewardManager Signed-off-by: GavinXu <meiyan532484710@gmail.com>

--- x/reward/contracts/sol/IRewardManager.sol
+++ x/reward/contracts/sol/IRewardManager.sol
@@ -7,10 +7,13 @@ interface IRewardManager {
     event ValidatorRewardWithdrawal(address indexed validator, uint256 amount, address caller);
     event DelegatorRewardWithdrawal(address indexed validator, uint256 amount, address caller);
 
-    /// @notice withdraws pending rewards for the sender (validator)
-    function withdrawValidatorReward(address validator) external; // only owner of validator call
+    /// @notice withdraws pending rewards for the sender (owner of validator)
+    /// @dev only owner of validator call
+    function withdrawValidatorRewards(address validator) external;
 
-    function withdrawDelegatorReward(address validator) external; // only delegator call
+    /// @notice withdraws pending rewards for the sender (delegator for validator)
+    /// @dev only delegator call
+    function withdrawDelegatorRewards(address validator) external;
 
     /// @notice returns the total reward (epoch reward and blocks reward) paid for the given epoch
     function paidRewardPerEpoch(uint256 epochId) external view returns (uint256);
@@ -18,5 +21,6 @@ interface IRewardManager {
     /// @notice returns the pending reward for the given account(validator)
     function pendingValidatorRewards(address validator) external view returns (uint256);
 
-    function pendingDelegatorRewards(address validator) external view returns (uint256);
+    /// @notice returns the pending reward of delegator for the given account(validator)
+    function pendingDelegatorRewards(address validator, address delegator) external view returns (uint256);
 }
