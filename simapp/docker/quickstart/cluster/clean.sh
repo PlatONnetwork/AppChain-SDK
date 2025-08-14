7abb9074cfb2eaf7ac787e90b871e39b7adcccdf
Tracebundy <yangzhou221@gmail.com>
2025-08-14 08:42:10 +0000

    delete simapp docker

--- simapp/docker/quickstart/cluster/clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-N=$1
-DATADIR=node$N
-rm -rf $DATADIR/data/platon
-rm -rf $DATADIR/data/sdk
-
--- simapp/docker/quickstart/cluster/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-N=$1
-node=node$N
-if [ ! -d $node/data/sdk ];
-then
-  simapp init --datadir $node/data ./genesis.json
-fi
